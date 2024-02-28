e56e70104af4eabdbf69e8e5dc55639414359b6d
bizumuremyi <bifofino@gmail.co>
2024-02-28 17:43:37 +0200

    craete javascript name

---
+++ java.js
@@ -0,0 +1,10 @@
+public class DisplayName {
+    public static void main(String[] args) {
+        // Replace "Your Name" with the actual name you want to display
+        String name = "Your Name";
+
+        // Display the name
+        System.out.println("Name: " + name);
+    }
+}
+
